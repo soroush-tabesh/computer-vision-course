1e7951a46a37aee30f00aa5314343a569fda64d1
Soroush Tabesh <soroushtabesh@gmail.com>
2021-04-05 22:20:06 +0430

    Small tweaks

--- hw1/q3_scratch.py
+++ hw1/q3_scratch.py
@@ -2,7 +2,6 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-from skimage import color, util
 
 
 def imshow(*srcs):
@@ -41,12 +40,12 @@ matches = bf.match(des1, des2)
 matches = sorted(matches, key=lambda x: x.distance)
 # %%
 
-img1_final_points = np.float32([kp1[m.queryIdx].pt for m in matches[:]])
-img2_final_points = np.float32([kp2[m.trainIdx].pt for m in matches[:]])
+img1_final_points = np.float32([kp1[m.queryIdx].pt for m in matches])
+img2_final_points = np.float32([kp2[m.trainIdx].pt for m in matches])
 
 M, mask = cv.findHomography(img2_final_points, img1_final_points, cv.RANSAC,
                             ransacReprojThreshold=3,
-                            maxIters=200000,
+                            maxIters=500000,
                             confidence=0.995)
 img4 = cv.warpPerspective(img2, M, img1.shape[:2][::-1])
 
@@ -58,5 +57,3 @@ img3 = cv.drawMatches(img1, kp1, img2, kp2, matches, None,
                       matchesMask=mask,  # draw only inliers
                       flags=2)
 imshow(img3)
-# %%
-matches = [matches[i] for i in range(len(matches)) if mask[i, 0] > 0]
